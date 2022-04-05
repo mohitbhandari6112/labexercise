c45be49e5df259fa9d55a9527b5619d70513fd76
mohit's mac <mac@MACs-MacBook-Air.local>
2022-04-05 23:23:19 +0545

    HelloToastLinearLayout

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.lab;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
