82bc00d746ef3ca09d5db8092b6f8953eb56fba2
mohit's mac <mac@MACs-MacBook-Air.local>
2022-04-05 22:33:17 +0545

    HelloConstraint

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -1,42 +1,67 @@
 package com.example.lab;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mButton;
-    private TextView mTextView;
-    private EditText mEdit;
-    int counter;
-    private static final String MAIN_ACTIVITY_STATE = "state";
-    String saveEdit;
+    private Button mToast, mCount, mZero;
+    private TextView mIncrement;
+    private int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = findViewById(R.id.button);
-        mTextView = findViewById(R.id.textView);
-        mEdit = findViewById(R.id.edit);
-        counter = 0;
-        mButton.setOnClickListener(this);
+        mToast = findViewById(R.id.toast);
+        mCount = findViewById(R.id.count);
+        mZero = findViewById(R.id.zero);
+        mIncrement = findViewById(R.id.increment);
 
-
+        mToast.setOnClickListener(this);
+        mCount.setOnClickListener(this);
+        mZero.setOnClickListener(this);
+        zeroEnable();
     }
 
     @Override
     public void onClick(View view) {
 
-        counter++;
-        mTextView.setText(""+counter);
+        switch (view.getId())
+        {
+            case R.id.count:
+                counter++;
+                zeroEnable();
+                mIncrement.setText(""+counter);
+                break;
+
+            case R.id.zero:
+                mIncrement.setText("0");
+                counter =0;
+                zeroEnable();
+                break;
+
+            case R.id.toast:
+                Toast.makeText( MainActivity.this, R.string.hello, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 
+    private void zeroEnable() {
+        if(counter==0)
+        {
+            mZero.setEnabled(false);
 
+        }
+        else
+            mZero.setEnabled(true);
+    }
 }
