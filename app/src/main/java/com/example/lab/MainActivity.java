43854e04fcade62cfc3ab87278b65922e7da7f3e
mohit's mac <mac@MACs-MacBook-Air.local>
2022-04-05 20:36:19 +0545

    ScrollingText2

--- app/src/main/java/com/example/lab/MainActivity.java
+++ app/src/main/java/com/example/lab/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
